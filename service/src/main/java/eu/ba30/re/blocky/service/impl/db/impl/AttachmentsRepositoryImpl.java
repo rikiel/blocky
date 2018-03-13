01008f7e4b2bb5d3718b47da19d576a6f9abb700
Richard Eliáš <richard.elias@matfyz.cz>
2018-03-13 23:22:50 +0100

    BLOCKY-7 Napojenie na databazu + mocky
    createAttachment

--- service/src/main/java/eu/ba30/re/blocky/service/impl/db/impl/AttachmentsRepositoryImpl.java
+++ service/src/main/java/eu/ba30/re/blocky/service/impl/db/impl/AttachmentsRepositoryImpl.java
@@ -15,23 +15,24 @@ import eu.ba30.re.blocky.service.impl.db.AttachmentsRepository;
 public class AttachmentsRepositoryImpl implements AttachmentsRepository {
     private static final String CREATE_ATTACHMENT_SQL_REQUEST = ""
                                                                 + " INSERT INTO T_ATTACHMENTS "
-                                                                + " (ID, NAME, FILE_NAME, MIME_TYPE, TYPE, FILE_CONTENT) "
-                                                                + " VALUES (?, ?, ?, ?, ?, ?) ";
+                                                                + " (ID, INVOICE_ID, NAME, FILE_NAME, MIME_TYPE, TYPE, FILE_CONTENT) "
+                                                                + " VALUES (?, ?, ?, ?, ?, ?, ?) ";
 
     @Autowired
     private JdbcTemplate jdbc;
 
     @Nonnull
     @Override
-    public List<Attachment> getAttachmentList(int invoiceId) {
+    public List<Attachment> getAttachmentList(final int invoiceId) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public void createAttachments(int invoiceId, @Nonnull List<Attachment> attachments) {
+    public void createAttachments(final int invoiceId, @Nonnull final List<Attachment> attachments) {
         final Attachment item = attachments.get(0);
         jdbc.update(CREATE_ATTACHMENT_SQL_REQUEST,
                 item.getId(),
+                invoiceId,
                 item.getName(),
                 item.getFileName(),
                 item.getMimeType(),
@@ -40,7 +41,7 @@ public class AttachmentsRepositoryImpl implements AttachmentsRepository {
     }
 
     @Override
-    public void removeAttachments(@Nonnull List<Integer> invoiceIds) {
+    public void removeAttachments(@Nonnull final List<Integer> invoiceIds) {
         throw new UnsupportedOperationException();
 
     }
