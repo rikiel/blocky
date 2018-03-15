5ad48913c223ca0a48e8265cc3e75fad7d79b243
Richard Eliáš <richard.elias@matfyz.cz>
2018-03-15 18:58:40 +0100

    BLOCKY-7 Napojenie na databazu + mocky
    zminimalizovanie pom zavislosti

--- common/src/main/java/eu/ba30/re/blocky/aspects/LoggingAspect.java
+++ common/src/main/java/eu/ba30/re/blocky/aspects/LoggingAspect.java
@@ -11,8 +11,10 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class LoggingAspect {
     private static final Logger log = LoggerFactory.getLogger(LoggingAspect.class);
 
