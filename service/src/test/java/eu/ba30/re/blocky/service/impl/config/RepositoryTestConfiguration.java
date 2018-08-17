edc4567dda3abba1bac03e050273c2032d9a230f
Richard Eliáš <richard.elias@matfyz.cz>
2018-08-17 09:46:08 +0200

    BLOCKY-12 Hibernate
    Hibernate - odstranenie myBatis zavislosti, repozitarov, mapperov

--- service/src/test/java/eu/ba30/re/blocky/service/impl/config/RepositoryTestConfiguration.java
+++
@@ -1,7 +0,0 @@
-package eu.ba30.re.blocky.service.impl.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public abstract class RepositoryTestConfiguration extends AbstractTestConfiguration {
-}
